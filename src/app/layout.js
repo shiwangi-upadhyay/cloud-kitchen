6a2d2e7914269412aee06c9b7ad6b4619a005a07
shiwangi-upadhyay <shiwangiupadhyay332@gmail.com>
2024-11-06 13:42:36 +0530

    Updated Files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+// Adding only the Roboto font
+const roboto = localFont({
+  src: "./fonts/roboto-regular-webfont.woff", // Make sure the path is correct
+  variable: "--font-roboto",
   weight: "100 900",
 });
 
@@ -20,9 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${roboto.variable} antialiased`}>
         {children}
       </body>
     </html>
