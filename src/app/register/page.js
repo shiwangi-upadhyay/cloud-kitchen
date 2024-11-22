42236817faa0d35ebf24ad40148bfacd354f9541
shiwangi-upadhyay <shiwangiupadhyay332@gmail.com>
2024-11-22 22:13:47 +0530

    updated some files

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,15 +1,24 @@
-export default function RegisterPage(){
-    return(
-        <section className="text-current text-primary
-     text-4xl  mt-8  ">
-        Register
-        <h1>
-            <form action="">
-                <input  type="email" placeholder="email"> </input>
-                <input  type="password" placeholder="password"> </input>
-                <button type="submit">Register</button>
-            </form>
-        </h1>
-     </section>
-    )
+import Image from "next/image";
+
+export default function RegisterPage() {
+    return (
+    <section className="mt-8">
+    <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+    <form className=" block max-w-xs mx-auto ">
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Register</button>
+        <div className=" my-4 text-center text-gray-500">
+
+            or login with provider
+        </div>
+        <button className="flex gap-4 justify-center "> 
+            <Image src={"/images/google.webp"} alt={' '} width={'32'} height={'32'} />
+            Login with google
+        </button>
+    </form>
+
+    
+    </section>
+    );
 }
